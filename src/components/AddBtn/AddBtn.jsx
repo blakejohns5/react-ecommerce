d1a524d85bfdde2019f991f7866ec77271f02a6e
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-07-06 09:32:33 +0200

    stable before shopping cart int

--- src/components/AddBtn/AddBtn.jsx
+++ src/components/AddBtn/AddBtn.jsx
@@ -1,15 +1,11 @@
 
 function AddBtn(product) {
 
-  function createProductContext () {  
-    console.log(product);
-  
-    return (
+  return (
     <>
-      <button className="btn__add btn btn-secondary font-weight-bold btn-md px-5 py-3 border rounded-pill fw-bold" onClick={ createProductContext }>Add to Cart</button>
+      <button className="btn__add btn btn-secondary font-weight-bold btn-md px-5 py-3 border rounded-pill fw-bold" >Add to Cart</button>
     </>    
-    )
-  }
+  )
 }
 
 export default AddBtn;
