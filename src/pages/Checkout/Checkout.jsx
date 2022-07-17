c2b403de5933e5c37ee64731fee26417a9962380
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-07-17 18:31:12 +0200

    fix top margin checkout

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -7,7 +7,7 @@ function Checkout({ cart, addToStorage, removeFromStorage, totalItems, totalCost
   if (cart.length === 0) {
     return (
       <>  
-    <main className='d-flex vh-100 flex-column col-8 justify-content-center align-items-start gap-5'>
+    <main className='d-flex vh-100 flex-column col-8 pt-5 mt-5 align-items-center gap-5'>
     <h1 className="mb-5 text-info fw-bold">Welcome to Checkout</h1>
     <h2 className="mb-5">You currently have no items in your shopping cart</h2>
     </main>
@@ -16,7 +16,7 @@ function Checkout({ cart, addToStorage, removeFromStorage, totalItems, totalCost
   } else {
     return (
       <> 
-       <main className='d-flex vh-100 flex-column col-10 justify-content-center align-items-start gap-5'>
+       <main className='d-flex vh-100 flex-column col-10 justify-content-start mt-5 pt-5 align-items-start gap-5'>
        <h1 className="mb-5 text-info fw-bold">Welcome to Checkout</h1>
         <section className="d-flex">
           <CheckoutTable cart={cart} addToStorage={addToStorage} removeFromStorage={removeFromStorage} />
