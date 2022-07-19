b3cbf6e8b1e3ee346c993e79eb09e3191b4a3f9f
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-07-19 18:07:19 +0200

    fix timer issue on order completion

--- src/components/OrderComplete/OrderComplete.jsx
+++ src/components/OrderComplete/OrderComplete.jsx
@@ -7,7 +7,8 @@ const OrderComplete = ({ setCheckoutStage }) => {
   const { setCartItems } = useContext(CartContext);
   const navigate = useNavigate();
 
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(6);
+
   useEffect(() => {
     if (count > 0) {
       setTimeout(() => setCount(count - 1), 1000);
@@ -26,7 +27,9 @@ const OrderComplete = ({ setCheckoutStage }) => {
       <main className='d-flex flex-column pt-5 mt-5 align-items-center gap-5'>
         <h1 className="text-info fw-bold">Thank you for your order.</h1>
         <h2 className="mb-5">You will receive confirmation by email soon.</h2>
-        <p className='mt-5 fs-3'>You will be redirected to the Product Page in <strong>{count}</strong> seconds.</p>
+        {count < 6 && (
+          <p className='mt-5 fs-3'>You will be redirected to the Product Page in <strong>{count}</strong> seconds.</p>
+        )}        
       </main>
     
     </>    
