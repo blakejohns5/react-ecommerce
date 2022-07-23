2dae6de6a38e9d781eed1d4f1f6d5d402c648f1b
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-07-23 13:26:11 +0200

    remove padding on dash photos

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -12,13 +12,13 @@ function ProductCard(props) {
   return (
     <>
       <div className="col-lg-4 p-2 ">
-        <div className="h-100 product__card col-lg-12 px-3 pt-3 pb-2 d-flex flex-column align-items-center text-center position-relative">
+        <div className="h-100 product__card col-lg-12  pb-2 d-flex flex-column align-items-center text-center position-relative">
             <div className="product__fav-btn">
               <FavBtn id={id} img={img} name={name} price={price} sale={sale} favState={favState} favDispatch={favDispatch}  />
             </div>
             <Link to={`/product/${id}`} className="text-decoration-none text-dark d-flex flex-column align-items-center text-center">
             <img src={img} alt="shoes_01" className="img-fluid mb-1" />
-            <div className="w-100 d-flex justify-content-between ">
+            <div className="w-100 d-flex justify-content-between px-2 ">
               <div className="my-2 text-decoration-none ms-1">{name}</div>  
               {/* Add icon and sale price if item on sale */}
               <div className="d-flex align-items-center me-1">
